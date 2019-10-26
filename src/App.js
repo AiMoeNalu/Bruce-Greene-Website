6a63e28591c74e846137433df2dadd9458e99fe8
AiMoeNalu <48229683+AiMoeNalu@users.noreply.github.com>
2019-10-26 18:51:13 -0400

    Setup for desktop development environment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <header></header>
+        <div className='content'></div>
+        <footer></footer>
       </div>
     );
   }
